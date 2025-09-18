56627567c5ace5c8be7dca68f38252f56f9cbad0
Prason <prasonkc07@gmail.com>
2025-09-18 22:15:44 +0545

    Created Routes

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,25 +1,19 @@
 import "./App.css";
-import Button from "./components/Button";
+import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import Login from "./components/Login";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Navbar />
-      <section className="bg-[#0F172A] min-h-[80vh] flex flex-col justify-center items-center text-center px-6">
-        <div className="max-w-3xl">
-          <h1 className="text-5xl md:text-6xl font-extrabold text-[#E2E8F0] leading-tight">
-            Fund Your Creative Work
-          </h1>
-          <h3 className="mt-4 text-xl md:text-2xl text-gray-400">
-            Accept support. Start a membership. Setup a shop. It’s easier than
-            you think.
-          </h3>
-          <div className="mt-8">
-            <Button text="Donate Now" />
-         </div>
-        </div>
-      </section>
+
+      {/* Routing Components */}
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
   );
 }
---
+++ app/src/components/Home.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Button from "./Button";
+
+
+const Home = () => {
+  return (
+    <section className="bg-[#0F172A] min-h-[80vh] flex flex-col justify-center items-center text-center px-6">
+      <div className="max-w-3xl">
+        <h1 className="text-5xl md:text-6xl font-extrabold text-[#E2E8F0] leading-tight">
+          Fund Your Creative Work
+        </h1>
+        <h3 className="mt-4 text-xl md:text-2xl text-gray-400">
+          Accept support. Start a membership. Setup a shop. It’s easier than you
+          think.
+        </h3>
+        <div className="mt-8">
+          <Button text="Donate Now" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
