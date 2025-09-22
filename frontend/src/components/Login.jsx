c94b0727d973a22a7f3e60cdf1238ec0a34f68df
Prason <prasonkc07@gmail.com>
2025-09-22 21:22:10 +0545

    Login Authentication

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,7 +1,26 @@
-import React from "react";
-import Button from "./Button";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  function handleClick(e) {
+    e.preventDefault();
+    fetch("http://localhost:4000/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
   return (
     <div className="min-h-screen flex items-center justify-center bg-[#0F172A]">
       <form className="bg-[#1E293B] p-8 rounded-2xl shadow-lg w-full max-w-md flex flex-col gap-6">
@@ -13,6 +32,9 @@ const Login = () => {
           id="email"
           placeholder="Email"
           className="w-full px-4 py-3 rounded-lg border border-gray-600 focus:outline-none focus:ring-2 focus:ring-[#38BDF8] focus:border-[#38BDF8] bg-[#0F172A] text-[#E2E8F0] placeholder-gray-400"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
         />
 
         <input
@@ -21,11 +43,15 @@ const Login = () => {
           id="password"
           placeholder="Password"
           className="w-full px-4 py-3 rounded-lg border border-gray-600 focus:outline-none focus:ring-2 focus:ring-[#38BDF8] focus:border-[#38BDF8] bg-[#0F172A] text-[#E2E8F0] placeholder-gray-400"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
         />
 
         <button
           type="submit"
           className="w-full py-3 bg-[#38BDF8] hover:bg-[#0EA5E9] text-[#0F172A] font-semibold rounded-lg shadow-md transition-colors hover:cursor-pointer"
+          onClick={handleClick}
         >
           Log In
         </button>
