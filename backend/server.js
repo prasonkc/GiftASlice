cd9b18b549992f5c5aeeea3b0d2f1c122af88ded
Prason <prasonkc07@gmail.com>
2025-09-22 21:22:10 +0545

    Login Authentication

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,65 @@
-const express = require("express")
-const cors = require("cors")
-const mongoose = require("mongoose")
-const Userbase = require("./userbase")
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const Userbase = require("./userbase");
 
-const app = express()
+const app = express();
+const PORT = 4000;
 
 // Middleware declaration
-app.use((cors()))
+app.use(cors());
+app.use(express.json());
 
 // Connect to database
 const dbURI = "mongodb://localhost:27017/DonationSite";
-mongoose.connect(dbURI).then((result) => {
+mongoose
+  .connect(dbURI)
+  .then((result) => {
     console.log("Connected to Mongoose Database");
 
     // Listen for requests
-    app.listen(3000);
-}).catch((e) => {
-    console.log(e)
-})
+    app.listen(PORT);
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 
 // Backend Routes
 app.get("/", (req, res) => {
-    res.json({"Message": "Backend Reachable"})
-})
+  res.json({ Message: "Backend Reachable" });
+});
+
+app.post("/signup", async (req, res) => {
+  try {
+    console.log("Post Request initiated");
+    const { name, dob, email, password } = req.body;
+
+    const databaseObject = new Userbase({ name, dob, email, password });
+    await databaseObject.save();
+
+    res.json({ message: "Registration successful" });
+  } catch (err) {
+    console.error(err);
+    res
+      .status(500)
+      .json({ message: "Registration failed", error: err.message });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  // Find the user by email address
+  const user = await Userbase.findOne({ email });
+
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+  // Check password
+  if (user.password !== password) {
+    return res.status(401).json({ message: "Incorrect password" });
+  }
+  res.json({ message: "Login successful" }).catch((e) => {
+    console.log(e);
+  });
+});
--- backend/userbase.js
+++ backend/userbase.js
@@ -1,16 +1,7 @@
 const mongoose = require("mongoose");
-const { type } = require("os");
 const Schema = mongoose.Schema;
 
 const LoginSchema = new Schema({
-    username: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
     name: {
         type: String,
         required: true
@@ -18,8 +9,16 @@ const LoginSchema = new Schema({
     dob: {
         type: Date,
         required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
     }
 }, {timestamps: true});
 
-const Blog = mongoose.model('Blog', blogSchema)
-module.exports = Blog
+const users = mongoose.model('Userbase', LoginSchema)
+module.exports = users
