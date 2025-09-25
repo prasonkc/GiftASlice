3f5735230716e11fc544adb9f9f10a915c721d3c
Prason <prasonkc07@gmail.com>
2025-09-25 17:45:47 +0545

    Changed user panel behaviour

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const Navbar = ({ loggedIn, user }) => {
+  const [userClick, setUserClick] = useState(false);
+  const panelRef = useRef(null);
+
+  function handleUserClick(event) {
+    // Change the state if panel is clicked
+        setUserClick(!userClick);
+  }
+
+  // Close the user panel if it is open
+   useEffect(() => {
+    function handleClickOutside(event) {
+      if (panelRef.current && !panelRef.current.contains(event.target)) {
+        setUserClick(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
     <>
-      <nav className="bg-[#0F172A] text-[#E2E8F0] px-6 py-4 flex items-center justify-between">
+      <nav className="bg-[#0F172A] text-[#E2E8F0] px-6 py-4 flex items-center justify-between mx-7">
         <Link to="/">
           {/* Left Icon */}
           <div className="text-xl font-bold">Icon</div>
         </Link>
 
-        {/*Right Panel  */}
+        {/*Search bar  */}
         <div className="flex items-center gap-6">
           <input
             type="text"
@@ -38,17 +61,30 @@ const Navbar = ({ loggedIn, user }) => {
 
           {/* User Panel */}
           {loggedIn && (
-            <div className="flex items-center gap-3 p-3 bg-[#1E293B] rounded-xl shadow-lg cursor-pointer">
+            <div 
+            className="flex flex-col items-center justify-center gap-2 bg-[#1E293B] rounded-2xl shadow-xl cursor-pointer relative"
+            ref={panelRef}>
               {/* Avatar */}
-              <div className="w-10 h-10 rounded-full bg-[#38BDF8] flex items-center justify-center text-[#0F172A] font-bold">
+              <div
+                className="w-12 h-12 rounded-full bg-[#38BDF8] flex items-center justify-center text-[#0F172A] font-bold text-lg hover:scale-105 transition-transform"
+                onClick={handleUserClick}
+              >
                 {user?.charAt(0).toUpperCase()}
                 {user?.charAt(1).toUpperCase()}
               </div>
 
-              {/* Username */}
-              {/* <div className="text-[#E2E8F0] font-medium">{user}</div> */}
+              {/* Panel */}
+              {userClick && (
+                <div className="absolute top-14 flex flex-col bg-[#2d3852] rounded-xl shadow-lg w-36 text-center">
+                  <ul className="flex flex-col text-[#E2E8F0] font-medium">
+                    <li className="hover:bg-[#1E293B] py-3 rounded">{user}</li>
+                    <li className="hover:bg-[#1E293B] py-3 rounded">Logout</li>
+                  </ul>
+                </div>
+              )}
             </div>
           )}
+
         </div>
       </nav>
     </>
